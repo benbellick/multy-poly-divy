bbd19919039d6ed4f56e3db5dc1eb195d96ca761
Ben Bellick <benbellick@protonmail.com>
2024-04-15 22:00:00 -0500

    wip: adding structure

--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,11 @@
 open React.Dom.Dsl
 open Html
 
-let%component make () = div [||] [React.string "Hello world from OCaml"]
+module Quotients = struct
+  let%component make ~quotients () =
+    let mk_label i = string @@ "q_" ^ string_of_int i ^ ": " in
+    let mk_q_input i _q = label [||] [ mk_label i; input [||] [] ] in
+    form [||] [ div [||] @@ CCList.mapi mk_q_input quotients ]
+end
+
+let%component make () = div [||] [ Quotients.make ~quotients:[ "a"; "b" ] () ]
