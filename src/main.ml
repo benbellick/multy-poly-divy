8bba3bd19598bf2596bf31ef68935c7b2a1b35fd
Ben Bellick <benbellick@protonmail.com>
2024-04-16 19:26:46 -0500

    Got something working...
    based on https://github.com/jchavarri/jsoo-react-realworld-example-app/blob/3d26ad7c0a74069ed3b051c70833c5eba59e74da/src/login.ml#L4C1-L4C102

--- src/main.ml
+++ src/main.ml
@@ -2,10 +2,30 @@ open React.Dom.Dsl
 open Html
 
 module Quotients = struct
-  let%component make ~quotients () =
+  let%component make ~quo_count () =
     let mk_label i = string @@ "q_" ^ string_of_int i ^ ": " in
-    let mk_q_input i _q = label [||] [ mk_label i; input [||] [] ] in
-    form [||] [ div [||] @@ CCList.mapi mk_q_input quotients ]
+    let q_inputs =
+      CCList.init quo_count (fun i -> label [||] [ mk_label i; input [||] [] ])
+    in
+    form [||] [ div [||] @@ q_inputs ]
 end
 
-let%component make () = div [||] [ Quotients.make ~quotients:[ "a"; "b" ] () ]
+external to_input_element :
+  Ojs.t -> Js_of_ocaml.Dom_html.inputElement Js_of_ocaml.Js.t = "%identity"
+
+let%component make () =
+  let quo_count, set_quo_count = React.use_state CCFun.(const 1) in
+  div [||]
+    [
+      input
+        [|
+          type_ "number";
+          value (string_of_int quo_count);
+          onChange (fun e ->
+              set_quo_count (fun _ ->
+                  (React.Event.Form.target e |> to_input_element)##.value
+                  |> Js_of_ocaml.Js.parseInt));
+        |]
+        [];
+      Quotients.make ~quo_count ();
+    ]
